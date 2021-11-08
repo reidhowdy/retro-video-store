ad6ca1060b3628ac21d3e4bcc2463bfb2e9f85c5
Reid Maute <reidhowdy@gmail.com>
2021-11-08 10:26:25 -0800

    completed GET and POST for /goals endpoint

--- app/models/customer.py
+++ app/models/customer.py
@@ -7,5 +7,23 @@ class Customer(db.Model):
     phone = db.Column(db.String)
     register_at = db.Column(db.DateTime)
 
+    def to_dict(self):
+        customer = {
+            "id" : self.customer_id,
+            "name": self.name,
+            "phone": self.phone,
+            "postal_code": self.postal_code
+        }
 
+        return customer
 
+    #why do we need this decorator
+    #why do we need to call Customer here and in routes
+    @classmethod
+    def from_dict(cls, request_body):
+        customer = Customer(
+            name=request_body["name"], 
+            postal_code=request_body["postal_code"],
+            phone=request_body["phone"]
+        )
+        return customer
--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,6 @@ from app.models.customer import Customer
 from app.models.video import Video
 from datetime import datetime
 from dotenv import load_dotenv
-
 load_dotenv()
 
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
@@ -16,12 +15,41 @@ def get_customers():
 
     response = []
     for customer in customers:
-        response.append({
-            "name": customer.name,
-            "id" : customer.customer_id,
-            "phone": customer.phone,
-            "postal_code": customer.postal_code
-        })
+        response.append(customer.to_dict())
 
     return jsonify(response), 200
+
+
+@customers_bp.route("", methods=["POST"])
+def create_customer():
+    request_body = request.get_json()
+    try:
+        new_customer = Customer.from_dict(request_body)
+        db.session.add(new_customer)
+        db.session.commit()
+
+        response = {
+            "id" : new_customer.customer_id,
+            "message" : "Customer successfully created."
+        }
+
+        return jsonify(response), 201
+    
+    except KeyError:
+        if "postal_code" not in request_body.keys():
+            response = {
+                "details" : "Request body must include postal_code."
+            }
+        elif "name" not in request_body.keys():
+            response = {
+                "details" : "Request body must include name."
+            }
+        elif "phone" not in request_body.keys():
+            response = {
+                "details" : "Request body must include phone."
+            }
+        
+        return jsonify(response), 400
+
+
 
